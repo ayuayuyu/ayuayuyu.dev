083ef590a7d0dfa3fd991991da17f1a0502dda29
ayu <umaminokiami@gmail.com>
2025-03-09 23:11:27 +0900

    Skillコンポーネントの追加

--- src/components/features/top/skills/index.tsx
+++ src/components/features/top/skills/index.tsx
@@ -1,19 +1,14 @@
-import { Icon } from '@iconify/react';
-import styles from './index.module.scss';
-import Title from '@/components/title';
 import { MAINSKILLS } from '@/constants/mainskill';
+import Title from '@/components/title';
+import Skill from '@/components/skill';
+import styles from './index.module.scss';
 
 const MainSkills = () => {
   return (
     <div>
       <Title>Skills</Title>
-      <div className={styles.gridContainer}>
-        {MAINSKILLS.map(({ icon, label, width, height }) => (
-          <div key={label} className={styles.iconContainer}>
-            <Icon icon={icon} width={width} height={height} />
-            <p>{label}</p>
-          </div>
-        ))}
+      <div className={styles.container}>
+        <Skill skills={MAINSKILLS}>Main</Skill>
       </div>
     </div>
   );
---
+++ src/components/skill/index.tsx
@@ -0,0 +1,31 @@
+import { Icon } from '@iconify/react/dist/iconify.js';
+import styles from './index.module.scss';
+
+type SkillItem = {
+  icon: string;
+  label: string;
+  width: string;
+  height: string;
+};
+type SkillProps = {
+  children: React.ReactNode;
+  skills: SkillItem[];
+};
+
+const Skill = ({ children, skills }: SkillProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{children}</div>
+      <div className={styles.gridContainer}>
+        {skills.map(({ icon, label, width, height }) => (
+          <div key={label} className={styles.iconContainer}>
+            <Icon icon={icon} width={width} height={height} />
+            <p>{label}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
