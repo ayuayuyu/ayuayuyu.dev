eeec651ba7e3c7a23651f22542a6f02d3b1832fe
ayu <umaminokiami@gmail.com>
2025-03-26 22:13:09 +0900

    add: favicon

---
+++ src/constants/link.ts
@@ -0,0 +1,7 @@
+export const GitLink: string = 'https://github.com/ayuayuyu';
+export const GitName: string = '@ayuayuyu';
+export const XLink: string = 'https://x.com/ayuayuyu07';
+export const XName: string = '@ayuayuyu07';
+export const QiitaLink: string = 'https://qiita.com/Ayuayuyu';
+export const QiitaName: string = '@Ayuayuyu';
+export const GmailLink: string = 'hanadaayumu78@gmail.com';
